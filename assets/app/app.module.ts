c792a91d9d40db93a0e01abd0f861d32577bda7b
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-01-31 18:21:53 -0500

    task: added template for books

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { BooksComponent } from "./books/books.component";
 
 @NgModule({
     declarations: [
 	    AppComponent,
 	    HeaderComponent,
-	    FooterComponent
+	    FooterComponent,
+	    BooksComponent
     ],
     imports: [BrowserModule],
     bootstrap: [AppComponent]
---
+++ assets/app/books/books.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'app-books',
+	template: 
+	`
+		TODO: implement routing here 
+	`
+})
+
+export class BooksComponent {
+	
+}
