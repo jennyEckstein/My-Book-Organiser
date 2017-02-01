42859837b3229ffff778e462d34154e64cd329ca
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-02-01 09:05:33 -0500

    feat: added routing to sign in and sign up components

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,12 +2,15 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { BooksComponent } from "./books/books.component";
 import { ProfileComponent } from "./books/profile.component";
-
+import { SigninComponent } from "./header/auth/signin.component";
+import { SignupComponent } from "./header/auth/signup.component";
 
 const APP_ROUTER: Routes = [
 	{path: '', redirectTo: '/books', pathMatch: 'full'},
 	{path: 'books', component: BooksComponent},
-	{path: 'profile', component: ProfileComponent}
+	{path: 'profile', component: ProfileComponent},
+	{path: 'signin', component: SigninComponent},
+	{path: 'signup', component: SignupComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTER);
