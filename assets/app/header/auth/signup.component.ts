d1e31a20bbec8457a20fa518498bb2728d374ab5
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2017-02-19 14:52:23 -0500

    feat: created front-end for the signup component

--- assets/app/header/auth/signup.component.ts
+++ assets/app/header/auth/signup.component.ts
@@ -1,10 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { 
+	FormGroup, 
+	FormControl, 
+	Validators, 
+	ReactiveFormsModule } from "@angular/forms";
+
 
 @Component ({
 	selector: 'app-signup',
-	template: `<p>Signup Component</p>`
+	templateUrl: 'signup.component.html'
 })
 
-export class SignupComponent {
-	
+export class SignupComponent implements OnInit{
+	signupForm: FormGroup;
+
+	onSubmit(){
+
+	}
+
+	ngOnInit(){
+		this.signupForm = new FormGroup({
+			firstName: new FormControl(null, Validators.required),
+			lastName: new FormControl(null, Validators.required),
+			email: new FormControl(null, Validators.required),
+			password: new FormControl(null, Validators.required)
+		});
+	}
 }
